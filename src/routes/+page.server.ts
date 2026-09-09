642064d9a16b4d32b297fc65c0cddc026eebaa0a
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2026-09-09 13:43:58 +0200

    chore: update deps, fix minor issues

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -12,13 +12,12 @@ export const load: PageServerLoad = async () => {
 	const BIRTHDAY = dayjs("2008-03-05");
 	const age = dayjs().diff(dayjs(BIRTHDAY), "year");
 
-	if (projects && buttons) {
-		return {
-			age: age,
-			buttons: buttons,
-			projects: projects,
-		};
-	}
-
+	return {
+		age: age,
+		buttons: buttons,
+		projects: projects,
+	};
+	
+	
 	error(404, "Not found");
 };
